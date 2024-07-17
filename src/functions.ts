7e17095b0d707c12f7fbeb409ca3a0df20acceb4
DEVDESIIGNN <muizharuna55@gmail.com>
2024-07-17 20:43:47 +0100

    refactor: added toLowerCase() to account for uppercase edge case

--- src/functions.ts
+++ src/functions.ts
@@ -3,7 +3,7 @@ import { NigeriaLocations } from "./data/nigeria-locations";
 
 function findSingleState(stateID: string): TStateInfo | undefined {
   return NigeriaLocations.find((NigeriaLocation) => {
-    return NigeriaLocation.state.id === stateID;
+    return NigeriaLocation.state.id === stateID.toLowerCase();
   })?.state;
 }
 
@@ -18,7 +18,7 @@ export function useAllStatesInfo(): TStateInfo[] {
 
 // FETCH ALL INFORMATION FROM SINGLE STATE (LOCATION ID)
 export function useStateInfo(stateID: string): TStateInfo | string {
-  return findSingleState(stateID) ?? "Not Found!, Check the ID Passed.";
+  return findSingleState(stateID.toLowerCase()) ?? "Not Found!, Check the ID Passed.";
 }
 
 // FETCH ALL STATES
@@ -33,7 +33,7 @@ export function useAllStates(): TState[] {
 
 // FETCH SINGLE STATE (LOCATION ID)
 export function useSingleState(stateID: string): TState | string {
-  const singleState = findSingleState(stateID);
+  const singleState = findSingleState(stateID.toLowerCase());
 
   if (singleState) return { id: singleState.id, name: singleState.name };
   else {
@@ -43,39 +43,39 @@ export function useSingleState(stateID: string): TState | string {
 
 // FETCH CAPITAL OF SINGLE STATE (LOCATION ID)
 export function useStateCapital(stateID: string): string {
-  return findSingleState(stateID)?.capital ?? "Not Found!, Check the ID Passed.";
+  return findSingleState(stateID.toLowerCase())?.capital ?? "Not Found!, Check the ID Passed.";
 }
 
 // FETCH ALL LGAs IN SINGLE STATE (LOCATION ID)
 export function useStateLGAs(stateID: string): TLGA[] | string {
-  return findSingleState(stateID)?.lgas ?? "Not Found!, Check the ID Passed.";
+  return findSingleState(stateID.toLowerCase())?.lgas ?? "Not Found!, Check the ID Passed.";
 }
 
 // FETCH SINGLE LGA (LOCATION ID)
 export function useSingleLGA(stateID: string, LGAID: string): TLGA | string {
   return (
-    findSingleState(stateID)?.lgas.find((singleStateLgas) => {
-      singleStateLgas.id === LGAID;
+    findSingleState(stateID.toLowerCase())?.lgas.find((singleStateLgas) => {
+      singleStateLgas.id === LGAID.toLowerCase();
     }) ?? "Not Found!, Check the ID Passed."
   );
 }
 
 // FETCH LAND MASS OF SINGLE STATE (LOCATION ID)
 export function useStateLandMass(stateID: string): string {
-  return findSingleState(stateID)?.land_mass ?? "Not Found!, Check the ID Passed.";
+  return findSingleState(stateID.toLowerCase())?.land_mass ?? "Not Found!, Check the ID Passed.";
 }
 
 // FETCH ALL UNIVERSITIES IN SINGLE STATE (LOCATION ID)
 export function useStateUnis(stateID: string): TUniversity[] | string {
-  return findSingleState(stateID)?.universities ?? "Not Found!, Check the ID Passed.";
+  return findSingleState(stateID.toLowerCase())?.universities ?? "Not Found!, Check the ID Passed.";
 }
 
 // FETCH ALL AIRPORTS IN SINGLE STATE (LOCATION ID)
 export function useStateAirports(stateID: string): TAirport[] | string {
-  return findSingleState(stateID)?.airports ?? "Not Found!, Check the ID Passed.";
+  return findSingleState(stateID.toLowerCase())?.airports ?? "Not Found!, Check the ID Passed.";
 }
 
 // FETCH GEO POLITCAL ZONE IN SINGLE STATE (LOCATION ID)
 export function useStateGeoPoli(stateID: string): string {
-  return findSingleState(stateID)?.geopolitical_zone ?? "Not Found!, Check the ID Passed.";
+  return findSingleState(stateID.toLowerCase())?.geopolitical_zone ?? "Not Found!, Check the ID Passed.";
 }
